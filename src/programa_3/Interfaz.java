37fd10dd071d68637d134a05adb5559692448388
J Angel Quezada <tenma4lonso@gmail.com>
2023-05-29 15:02:12 -0600

    final

--- src/programa_3/Interfaz.java
+++ src/programa_3/Interfaz.java
@@ -1,26 +1,66 @@
-package tema_2;
+package programa_3;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Frame;
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
-
-public class Interfaz extends JFrame {
-    JPanel panel = new JPanel();
-    public Interfaz(){
-    pack();
-    setLocationRelativeTo(null);
-    setMinimumSize(new Dimension(200,200));
-    setSize(600, 600);
-    setVisible(true);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    componentePanel();
-    }
+public class Interfaz extends JFrame implements ActionListener{
+//    private FileNameExtensionFilter filter = new FileNameExtensionFilter(".jpg","png", "jpg");
+    JButton button;
+    JLabel label;
+    JFileChooser fileChooser;
     
-    private void componentePanel(){
-        this.getContentPane().add(panel);
-        panel.setBackground(Color.red);
+    public Interfaz(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+        this.setSize(500,600);
+        
+        label = new JLabel();
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setSize(100,100);
+        label.setVisible(true);
+//        label.setIcon(new ImageIcon(getClass().getResource("/imagenes/FsAyE3_XgAEhQTJ.jpg")));
+//        label.setIcon(new ImageIcon(getClass().getResource("/imagenes/")));
+//      ..\\imagenes\\FsAyE3_XgAEhQTJ.jpg
+
+        
+        button = new JButton();
+        button.setText("Buscar imagen");
+        button.setLayout(new GridBagLayout());
+        button.setFocusable(false);
+        button.addActionListener((ActionListener) this);
+        button.setVisible(true);
+
+        
+        this.setVisible(true);
+        this.add(button);
+        this.add(label);
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+       
+        if (e.getSource()==button) {
+            fileChooser = new JFileChooser();
+            fileChooser.setDialogTitle("Buscar imagen...");
+            fileChooser.setFileFilter(new FileNameExtensionFilter(".jpg", "jpg"));
+            fileChooser.setFileFilter(new FileNameExtensionFilter(".png", "png"));
+            fileChooser.setFileFilter(new FileNameExtensionFilter(".gif", "gif"));
+            fileChooser.setCurrentDirectory(new File("/imagenes/"));
+            int response = fileChooser.showOpenDialog(null);
+            if (response == JFileChooser.APPROVE_OPTION) {
+                label.setIcon(new ImageIcon(fileChooser.getSelectedFile().getAbsolutePath()));
+            }
+        }
     }
 }
--- src/programa_3/Main.java
+++ src/programa_3/Main.java
@@ -1,9 +1,12 @@
-package tema_2;
+package programa_3;
+
+
 
 public class Main {
 
     public static void main(String[] args) {
         Interfaz interfaz = new Interfaz();
+
 
     }
     
